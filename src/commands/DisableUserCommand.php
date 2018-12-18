e46187f404d8d03690820146f3c14953aabf6d96
Tomas Majer <tomasmajer@gmail.com>
2018-12-18 11:18:26 +0100

    Disable user command

---
+++ src/commands/DisableUserCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Crm\UsersModule\Commands;
+
+use Crm\UsersModule\Repository\UsersRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DisableUserCommand extends Command
+{
+    private $usersRepository;
+
+    public function __construct(
+        UsersRepository $usersRepository
+    ) {
+        parent::__construct();
+        $this->usersRepository = $usersRepository;
+    }
+
+    protected function configure()
+    {
+        $this->setName('user:disable')
+            ->setDescription('Disable user')
+            ->addArgument(
+                'email',
+                InputArgument::REQUIRED,
+                'User email'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+
+        $user =  $this->usersRepository->getByEmail($email);
+        if (!$user) {
+            $output->writeln("<error>User not found</error>");
+            return;
+        }
+
+        if (!$user->active) {
+            $output->writeln("<error>Inactive user</error>");
+            return;
+        }
+
+        $this->usersRepository->toggleActivation($user);
+        $output->writeln("Done");
+    }
+}
