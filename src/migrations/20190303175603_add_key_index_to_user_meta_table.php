ca371d8033df1124ebda09f6c61374a60df6da9b
Miroslav Svítok <mirosvitok@gmail.com>
2019-03-03 19:26:58 +0100

    Admin payments dashboard optimizations by caching, query rewrite and added index

---
+++ src/migrations/20190303175603_add_key_index_to_user_meta_table.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddKeyIndexToUserMetaTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('user_meta')
+            ->addIndex('key')
+            ->save();
+    }
+}
