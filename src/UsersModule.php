00e95fa18940aacf46aa1beb7cf3068fa4474b11
Miroslav Svítok <mirosvitok@gmail.com>
2019-03-06 14:32:23 +0100

    Minor fixes, refactoring

--- src/UsersModule.php
+++ src/UsersModule.php
@@ -348,7 +348,7 @@ class UsersModule extends CrmModule
     public function cache(OutputInterface $output, array $tags = [])
     {
         if (in_array('precalc', $tags, true)) {
-            $output->writeln('* Refreshing <info>users stats</info> cache');
+            $output->writeln('  * Refreshing <info>users stats</info> cache');
 
             $this->usersRepository->totalCount(true, true);
         }
