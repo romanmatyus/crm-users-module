8fb890581f31faf06b86f957f266f1e9446f8efe
Dominik Skerhak <dominik.skerhak@gmail.com>
2019-05-28 12:35:48 +0200

    Remove user tokens if user was deactivated
    Still present invalid `n_token` was loaded from cookie by FrontedPresenter
    and it tried to login user. ContentAuthenticator than returned warning
    message (until new registration or manual n_token removal).
    
    remp/crm#670

--- src/events/AuthenticationHandler.php
+++ src/events/AuthenticationHandler.php
@@ -8,6 +8,8 @@ use Crm\UsersModule\Repository\AccessTokensRepository;
 use Crm\UsersModule\Repository\UsersRepository;
 use League\Event\AbstractListener;
 use League\Event\EventInterface;
+use Nette\Http\Request;
+use Nette\Http\Response;
 use Nette\Localization\ITranslator;
 use Nette\Security\AuthenticationException;
 
@@ -19,17 +21,25 @@ class AuthenticationHandler extends AbstractListener
 
     private $accessTokensRepository;
 
+    private $request;
+
+    private $response;
+
     private $translator;
 
     public function __construct(
         UsersRepository $usersRepository,
         AccessToken $accessToken,
         AccessTokensRepository $accessTokensRepository,
+        Request $request,
+        Response $response,
         ITranslator $translator
     ) {
         $this->usersRepository = $usersRepository;
         $this->accessToken = $accessToken;
         $this->accessTokensRepository = $accessTokensRepository;
+        $this->request = $request;
+        $this->response = $response;
         $this->translator = $translator;
     }
 
@@ -41,6 +51,8 @@ class AuthenticationHandler extends AbstractListener
 
         $user = $this->usersRepository->find($event->getUserId());
         if (!$user->active) {
+            $this->accessTokensRepository->removeAllUserTokens($user->id);
+            $this->accessToken->deleteActualUserToken($user, $this->request, $this->response);
             throw new AuthenticationException($this->translator->translate('users.authenticator.inactive_account'));
         }
 
