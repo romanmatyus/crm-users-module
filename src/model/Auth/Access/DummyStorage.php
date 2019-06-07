a5818bec76e1ae01ba8bf918ecb6010cacb650ed
Miroslav Svítok <mirosvitok@gmail.com>
2019-06-07 14:05:31 +0200

    Refactoring
    remp/crm#683

--- src/model/Auth/Access/DummyStorage.php
+++ src/model/Auth/Access/DummyStorage.php
@@ -2,10 +2,7 @@
 
 namespace Crm\UsersModule\Auth\Access;
 
-use Crm\ContentModule\Access\ContentShareInterface;
-use Crm\ContentModule\Access\UrlShareInterface;
-
-class DummyStorage implements StorageInterface, ContentShareInterface, UrlShareInterface
+class DummyStorage implements StorageInterface
 {
     public function addToken($token, $type = 'access')
     {
@@ -23,50 +20,6 @@ class DummyStorage implements StorageInterface, ContentShareInterface, UrlShareI
     }
 
     public function allTokens($type = 'access')
-    {
-        return true;
-    }
-
-    public function getUrls($key)
-    {
-        return true;
-    }
-
-    public function addUrl($key, $url)
-    {
-        return true;
-    }
-
-    public function removeUrl($key, $url)
-    {
-        return true;
-    }
-
-    public function urlExists($key, $url)
-    {
-        return true;
-    }
-
-
-    public function addContentShareToken($url, $token)
-    {
-        return true;
-    }
-
-
-    public function existsContentShareToken($url, $token)
-    {
-        return true;
-    }
-
-
-    public function removeContentShareToken($url, $token)
-    {
-        return true;
-    }
-
-
-    public function getContentUse()
     {
         return true;
     }
