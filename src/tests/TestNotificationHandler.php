0511b21a5713840e6eaa9c2d7dd2a75b474465b0
Miroslav Svítok <mirosvitok@gmail.com>
2020-04-02 18:37:18 +0200

    Add donated email to gift notification
    remp/crm#561

---
+++ src/tests/TestNotificationHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Crm\UsersModule\Tests;
+
+use Crm\UsersModule\Events\NotificationEvent;
+use League\Event\AbstractListener;
+use League\Event\EventInterface;
+
+class TestNotificationHandler extends AbstractListener
+{
+    /** @var []NotificationEvent  */
+    private $notifications = [];
+
+    public function handle(EventInterface $event)
+    {
+        if (!($event instanceof NotificationEvent)) {
+            throw new \Exception('Unable to handle event, expected NotificationEvent');
+        }
+
+        $email = $event->getUser()->email;
+
+        if (!array_key_exists($email, $this->notifications)) {
+            $this->notifications[$email] = [];
+        }
+
+        $this->notifications[$email][] = $event;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return NotificationEvent[]
+     */
+    public function notificationsSentTo(string $email): array
+    {
+        return $this->notifications[$email] ?? [];
+    }
+}
