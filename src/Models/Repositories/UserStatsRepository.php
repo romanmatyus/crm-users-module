3d87dc61e29ba2c92ff38a064bbd6ca89e4dc2f6
Miroslav Svitok <mirosvitok@gmail.com>
2022-07-04 13:57:41 +0000

    Moves user stats from user_meta to user_stats table
    remp/crm#2207

---
+++ src/Models/Repositories/UserStatsRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Crm\UsersModule\Repository;
+
+use Crm\ApplicationModule\Repository;
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+use Nette\Utils\DateTime;
+
+class UserStatsRepository extends Repository
+{
+    protected $tableName = 'user_stats';
+
+    final public function upsertUsersValues(string $key, array $usersValues, ?DateTime $now = null): void
+    {
+        if (!$now) {
+            $now = new DateTime();
+        }
+        foreach (array_chunk($usersValues, 5000, true) as $countsChunk) {
+            $data = [];
+            foreach ($countsChunk as $userId => $count) {
+                $data[] = [
+                    'created_at' => $now,
+                    'updated_at' => $now,
+                    'value' => $count,
+                    'user_id' => $userId,
+                    'key' => $key,
+                ];
+            }
+            $this->database->query(
+                'INSERT INTO `user_stats`',
+                $data,
+                'ON DUPLICATE KEY UPDATE `value`= VALUES(`value`), updated_at = VALUES(updated_at)'
+            );
+        }
+    }
+
+    final public function userStats($user): Selection
+    {
+        if ($user instanceof ActiveRow) {
+            $user = $user->id;
+        }
+        return $this->getTable()->where(['user_id' => $user])->order('key ASC');
+    }
+}
---
+++ src/migrations/20220622104724_create_user_stats_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUserStatsTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('user_stats')
+            ->addColumn('user_id', 'integer', ['null' => false])
+            ->addColumn('key', 'string', ['null' => false])
+            ->addColumn('value', 'string', ['null' => false])
+            ->addColumn('created_at', 'datetime', ['null' => false])
+            ->addColumn('updated_at', 'datetime', ['null' => false])
+            ->addForeignKey('user_id', 'users', 'id')
+            ->addIndex(['key, value'], ['unique' => true])
+            ->create();
+    }
+}
