36b4483aa38d6c3152b2245da793be49ecf14bbd
Peter Dulacka <dulacka@gmail.com>
2022-12-01 13:06:56 +0000

    Log user out of CRM if there's no n_token in the cookie
    We were able to log user out if the token was present and invalid,
    but the scenario with no token at all was not covered.
    
    remp/helpdesk#1639

--- src/Events/AuthenticationHandler.php
+++ src/Events/AuthenticationHandler.php
@@ -15,32 +15,14 @@ use Nette\Security\AuthenticationException;
 
 class AuthenticationHandler extends AbstractListener
 {
-    private $usersRepository;
-
-    private $accessToken;
-
-    private $accessTokensRepository;
-
-    private $request;
-
-    private $response;
-
-    private $translator;
-
     public function __construct(
-        UsersRepository $usersRepository,
-        AccessToken $accessToken,
-        AccessTokensRepository $accessTokensRepository,
-        Request $request,
-        Response $response,
-        Translator $translator
+        private UsersRepository $usersRepository,
+        private AccessToken $accessToken,
+        private AccessTokensRepository $accessTokensRepository,
+        private Request $request,
+        private Response $response,
+        private Translator $translator
     ) {
-        $this->usersRepository = $usersRepository;
-        $this->accessToken = $accessToken;
-        $this->accessTokensRepository = $accessTokensRepository;
-        $this->request = $request;
-        $this->response = $response;
-        $this->translator = $translator;
     }
 
     public function handle(EventInterface $event)
@@ -59,6 +41,9 @@ class AuthenticationHandler extends AbstractListener
         $token = $this->accessToken->getToken($event->getRequest());
         if ($token && !$this->accessTokensRepository->loadToken($token)) {
             $this->accessToken->deleteActualUserToken($user, $this->request, $this->response);
+            throw new AuthenticationException($this->translator->translate('users.frontend.sign_in.signed_out'));
+        }
+        if (!$token) {
             throw new AuthenticationException($this->translator->translate('users.frontend.sign_in.signed_out'));
         }
     }
